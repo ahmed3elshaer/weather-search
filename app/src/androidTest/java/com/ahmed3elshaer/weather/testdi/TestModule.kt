ae6734dc3e4c427302d11fb136093f8e1830bfe2
MageBuild <ahmed3elshaer@gmail.com>
2024-02-15 22:42:27 +0200

    Remove unused layout, clean testing modules

---
+++ app/src/androidTest/java/com/ahmed3elshaer/weather/testdi/TestModule.kt
@@ -0,0 +1,10 @@
+package com.ahmed3elshaer.weather.testdi
+
+//@Module
+//@TestInstallIn(
+//    components = [SingletonComponent::class],
+//    replaces = [DataModule::class]
+//)
+interface FakeDataModule {
+
+}
